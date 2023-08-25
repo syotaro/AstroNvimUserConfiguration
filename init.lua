2fabf6bbad28a26d9485e25c4d97bde6df2786d0
syotaro <y@syotaro.biz>
2023-08-26 00:06:44 +0900

    fix

--- init.lua
+++ init.lua
@@ -16,13 +16,16 @@ return {
       --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
     },
   },
+
   -- Set colorscheme to use
   colorscheme = "astrodark",
+
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = true,
     underline = true,
   },
+
   lsp = {
     -- customize lsp formatting options
     formatting = {
@@ -50,6 +53,7 @@ return {
       -- "pyright"
     },
   },
+
   -- Configure require("lazy").setup() options
   lazy = {
     defaults = { lazy = true },
@@ -60,6 +64,7 @@ return {
       },
     },
   },
+
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
@@ -78,3 +83,4 @@ return {
     -- }
   end,
 }
+
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,7 +1,8 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
+  opts = function(_, opts)
+    -- add more things to the ensure_installed table protecting against community packs modifying it
+    opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
       "bash",
       "css",
       "dockerfile",
@@ -27,6 +28,6 @@ return {
       "prisma",
       "php",
       "rust",
-    },
-  },
+    })
+  end,
 }
